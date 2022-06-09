e8442c93dcfa92f619da55e956295d117ff8c34d
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-06-09 13:07:14 -0500

    Added a POST route to the router

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,49 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// POST grocItem to the Database
+router.post('/', (req, res) => {
+    
+    // Pull out the request object as a variable
+    const newGrocItem = req.body
+
+    // Build the SQL statement using all the fields in the table
+    // regardless if they are going to be populated with values
+    // or with `null` (meaning nothing was provided).
+    const sqlText = `
+        INSERT INTO "shopping_list"
+            ("name", "quantity", "unit")
+        VALUES
+            ($1, $2, $3)
+    `
+
+    // Set the default unit measurement to null, in case
+    // the value is `undefined`.
+    let unitNumber = null
+    // If the value is NOT `undefined`, then set it to
+    // the provided value.
+    if (newGrocItem.unit) {
+        unitNumber = newGrocItem.unit
+    }
+
+    const sqlParams = [
+        newGrocItem.name,       // Name of the item
+        newGrocItem.quantity,   // Item count
+        unitNumber              // Unit of measurement (i.e. boxes), or `null`
+    ]
+
+    pool.query(sqlText, sqlParams)
+        .then((result) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log(`
+                Error making database query ${sqlText}
+            
+                ${err}`
+            )
+            res.sendStatus(500);
+        });
+});
 
 module.exports = router;
