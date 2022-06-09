62a9ca5ca6f2adc9c038c20050fb707053a3e7e1
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-06-09 10:26:43 -0500

    Updated the entities names within the app. Added a few extra fields

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
+// Import the modules from here
+import MainComponent from '../MainGrocItemContainer/GrocItemMainComponent.jsx';
+
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
+            <MainComponent />
         </div>
     );
 }
