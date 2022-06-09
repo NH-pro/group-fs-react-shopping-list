90ea27b1e6da2e4af9ebcb5287c478d7e1aba990
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-09 10:34:14 -0500

    created form setup

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+
+# Database
+To initalize the project's database execute the database.sql file.
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
+import GrocItemForm from '../GrocItemForm/GrocItemForm'
 
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <GrocItemForm />
             <main>
                 <p>Under Construction...</p>
             </main>
