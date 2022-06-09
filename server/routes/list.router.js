71be78e5f54e3e6e7b6edd16b81c1045b5214195
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-09 11:42:33 -0500

    created GET router

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,18 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// GET route to fetch all GrocItems from Database!
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM "shopping_list"`;
+    pool.query(sqlText)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        });
+});
+
 
 module.exports = router;
