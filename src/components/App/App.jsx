dc04fc0ab605f1076d00701b591e8391b1d7fb1e
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-06-09 13:48:05 -0500

    Added a POST route to the the App.jsx file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,41 @@
+// Core imports
 import React from 'react';
+import axios from 'axios';
 
-import Header from '../GrocItemHeader/GrocItemHeader'
+// Import App Stylesheet
 import './App.css';
-import GrocItemForm from '../GrocItemForm/GrocItemForm'
+
 
 // Import the modules from here
+import Header from '../GrocItemHeader/GrocItemHeader'
+import GrocItemForm from '../GrocItemForm/GrocItemForm'
 import GrocItemMainComponent from '../MainGrocItemContainer/GrocItemMainComponent.jsx';
 
 
+// The main App container for our React app.
+// Contains ALL the components that comprise our core React app within it.
 function App() {
+
+    // ----------------------------------------
+    // ROUTE SECTION - Communicates with server
+    // ----------------------------------------
+
+    // Function for to POST a new grocery item
+    const postGrocItem = (grocItemObj) => {
+        console.log("In the POST route with", grocItemObj)
+
+        axios.post("/", grocItemObj)
+        .then(() => console.log("Successful POST of new grocery item"))
+        .catch(err => {
+            console.log(`
+                Error in POST route in App.jsx:
+            
+                ${err}
+            `)
+        })
+    }
+
+
     return (
         <div className="App">
             <Header />
